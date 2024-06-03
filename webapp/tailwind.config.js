d50f2d7dcb855ae92e1995750a340c90d6f32ff3
Aniket Singh <thor98571@gmail.com>
2024-06-03 16:44:59 +0530

    feat:completed mobile designs of below sections: Democratic,community,footer and buy

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-    
     },
     fontSize:{
       'header':"55px",
-      "para":"16px"
+      "para":"16px",
+      'mobile-header':"45px"
     },
     colors:{
       'black-text':"#333333",
@@ -24,7 +24,8 @@ module.exports = {
       'subtext-three':'#777777',
       'card-heading':"#454545",
       'footer-text':'#979797',
-      'grey-text': '#c2c2c2'
+      'grey-text': '#c2c2c2',
+      'copyright-text':'#393939'
      },
      fontFamily: {
         "Sequel-Sans-Light-Body":["Sequel-Sans-Light-Body"],
@@ -58,6 +59,8 @@ module.exports = {
       // => @media (max-width: 1023px) { ... }
 
       'md': '767px',
+      
+      'lm':"400px",
       // => @media (max-width: 767px) { ... }
 
       'sm': '0px',
@@ -67,8 +70,10 @@ module.exports = {
       //for containing the elements at larger devices
       'center-width':'1170px'
     },
+   
     width:{
-      'section-width':"80%"
+      'section-width':"80%",
+      'mobile-section-width':"90%"
     },
     backgroundColor:{
       'white-background':"#ffffff",
