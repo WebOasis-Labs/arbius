03f04a3e045aa698fb0caccf6c078e8567ad917e
Abhishek <munjalabhishek1996@gmail.com>
2024-09-02 08:29:55 +0530

    Added network switch to mainnet and request account for auto connecting in chrome

--- website/src/app/stake/aius/gysr/middleware.jsx
+++ website/src/app/stake/aius/gysr/middleware.jsx
@@ -5,23 +5,34 @@ import { useState } from 'react';
 import fetchData from "../../../Utils/getGysrData";
 import Tabs from "@/app/components/Stake/GYSR/Tabs";
 import TopHeaderSection from "@/app/components/Stake/GYSR/TopHeaderSection";
+import { useSwitchNetwork } from 'wagmi';
+
 const Middleware = () => {
     const [data, setData] = useState(null)
+    const CHAIN = process?.env?.NEXT_PUBLIC_AIUS_ENV === "dev" ? 1 : 1;
+    const { switchNetwork: switchNetworkMainnet } = useSwitchNetwork({
+      chainId:CHAIN,
+    });
+
     useEffect(() => {
-     
-    
-    const getData = async () => {
-      try {
-        const result = await fetchData();
-        setData(result.data)
-        // setData(result);
-      } catch (error) {
-        // setError(error);
-      }
-    };
+      const getData = async () => {
+        try {
+          const result = await fetchData();
+          setData(result.data)
+          // setData(result);
+          const check = await window.ethereum.request({ method: 'eth_accounts' }); // Request account access if needed
+          if(check.length){
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+          }
+        } catch (error) {
+          // setError(error);
+        }
+      };
   
-    getData();
-  }, []);
+      getData();
+      switchNetworkMainnet?.();
+
+  }, [switchNetworkMainnet]);
   return (
     <div>
         <>
