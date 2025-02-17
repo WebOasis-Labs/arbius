d9f178a0efc4d831d84ab13d1019e92c016ea1ee
Abhishek Munjal <13870341+AbhiKakarot@users.noreply.github.com>
2025-02-17 20:56:17 +0530

    Added dashboard / gauge tab acc to mobile design

--- website/src/app/components/Stake/AIUS/Tabs.jsx
+++ website/src/app/components/Stake/AIUS/Tabs.jsx
@@ -20,13 +20,13 @@ function Tabs({
   console.log(updateValue, 'Value updated in TABS');
   return (
     <>
-      <div className='m-[auto] w-mobile-section-width max-w-center-width lg:w-section-width'>
+      <div className='m-[auto] md:w-mobile-section-width md:max-w-center-width lg:w-section-width'>
         <div className='flex justify-start'>
           <div className='all-stake stake-items flex w-full justify-between font-[600] lg:w-[30%]'>
             {tabs.map(function (item, index) {
               return (
                 <div
-                  className={` ${selectedtab === item ? 'selected' : 'non-selected'} hover:text-purple-text`}
+                  className={` ${selectedtab === item ? 'selected mobile-selected-background md:bg-none' : 'non-selected'} md:hover:text-purple-text`}
                   onClick={() => {
                     setSelectedTab(item);
                   }}
