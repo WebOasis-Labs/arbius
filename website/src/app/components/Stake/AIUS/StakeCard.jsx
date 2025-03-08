631f3c495cf05af75dd344fa10bd7aad1ed47875
Abhishek Munjal <13870341+AbhiKakarot@users.noreply.github.com>
2025-03-08 17:42:19 +0530

    Corrected extend button, it was greyed out incorrectly

--- website/src/app/components/Stake/AIUS/StakeCard.jsx
+++ website/src/app/components/Stake/AIUS/StakeCard.jsx
@@ -377,10 +377,10 @@ function StakeCard({
                   setShowPopUp('extend');
                   setSelectedStake(token?.tokenID);
                 } : null }
-                className={`group relative flex w-full items-center justify-center gap-3 rounded-full ${extendMonths && 0 ? "bg-black-background" : "bg-light-gray-background"} px-3 py-1 py-2 lg:px-4`}
+                className={`group relative flex w-full items-center justify-center gap-3 rounded-full ${extendMonths ? "bg-black-background" : "bg-light-gray-background"} px-3 py-1 py-2 lg:px-4`}
               >
                 <div className='absolute left-0 z-0 h-[100%] w-[100%] rounded-full bg-buy-hover px-4 py-2 opacity-0 transition-opacity duration-500 group-hover:opacity-100'></div>
-                <div className={`lato-bold relative z-10 ${extendMonths && 0 ? "text-original-white" : "text-black-text text-opacity-40"} lg:text-[15px]`}>
+                <div className={`lato-bold relative z-10 ${extendMonths ? "text-original-white" : "text-black-text text-opacity-40"} lg:text-[15px]`}>
                   Extend
                 </div>
               </button>
