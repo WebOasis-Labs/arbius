3082c4de9810054fd7daf604e24a9a4c1a6ae90e
Abhishek Munjal <13870341+AbhiKakarot@users.noreply.github.com>
2025-04-01 11:04:21 +0530

    Fixed merge conflicts

--- website/src/app/components/Stake/AIUS/StakeCard.jsx
+++ website/src/app/components/Stake/AIUS/StakeCard.jsx
@@ -377,7 +377,7 @@ function StakeCard({
                   setShowPopUp('extend');
                   setSelectedStake(token?.tokenID);
                 } : null }
-                className={`group relative flex w-full items-center justify-center gap-3 rounded-full ${extendMonths && 0 ? "bg-black-background" : "bg-light-gray-background"} px-3 py-1 py-2 lg:px-4`}
+                className={`group relative flex w-full items-center justify-center gap-3 rounded-full ${extendMonths ? "bg-black-background" : "bg-light-gray-background"} px-3 py-1 py-2 lg:px-4`}
               >
                 <div className='absolute left-0 z-0 h-[100%] w-[100%] rounded-full bg-buy-hover px-4 py-2 opacity-0 transition-opacity duration-500 group-hover:opacity-100'></div>
                 <div className={`md:lato-bold relative z-10 ${extendMonths ? "text-original-white" : "text-black-text text-opacity-40"} text-[13px] md:text-[15px]`}>
